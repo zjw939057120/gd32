535a63b0395976b8a9082dbb7bb9e654dbe694ac
zjw939057120 <zjw939057120@outlookcom>
2025-01-23 12:19:54 +0800

    初始化代码

---
+++ Core/Src/main.c
@@ -0,0 +1,105 @@
+/*!
+    \file    main.c
+    \brief   led spark with systick, USART print and key example
+
+    \version 2017-02-10, V1.0.0, firmware for GD32F30x
+    \version 2018-10-10, V1.1.0, firmware for GD32F30x
+    \version 2018-12-25, V2.0.0, firmware for GD32F30x
+    \version 2020-09-30, V2.1.0, firmware for GD32F30x 
+*/
+
+/*
+    Copyright (c) 2020, GigaDevice Semiconductor Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors 
+       may be used to endorse or promote products derived from this software without 
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+*/
+
+#include "gd32f30x.h"
+#include "systick.h"
+#include <stdio.h>
+#include "main.h"
+#include "SEGGER_RTT.h"
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+
+int main(void)
+{
+    SEGGER_RTT_WriteString(0, "SEGGER Real-Time-Terminal Sample\r\n");
+
+    /* configure systick */
+    systick_config();
+
+    init_GPIO();
+
+    init_UART();
+    
+    while(1){
+        delay_1ms(1000);
+        SEGGER_RTT_WriteString(0, "SEGGER Real-Time-Terminal Sample\r\n");
+        printf("a usart transmit test example!\n\r");
+    };
+}
+
+/* retarget the C library printf function to the USART */
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(USART1, (uint8_t)ch);
+    while(RESET == usart_flag_get(USART1, USART_FLAG_TBE));
+
+    return ch;
+}
+
+void init_GPIO()
+{
+    rcu_periph_clock_enable(RCU_GPIOC);
+    gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
+    gpio_bit_set(GPIOC, GPIO_PIN_9);
+}
+
+void init_UART()
+{
+    /* enable GPIO clock */
+    rcu_periph_clock_enable(RCU_GPIOA);
+
+    /* enable USART clock */
+    rcu_periph_clock_enable(RCU_USART1);
+
+    /* connect port to USARTx_Tx */
+    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_2);
+
+    /* connect port to USARTx_Rx */
+    gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_3);
+
+    /* USART configure */
+    usart_deinit(USART1);
+    usart_baudrate_set(USART1, 115200U);
+    usart_receive_config(USART1, USART_RECEIVE_ENABLE);
+    usart_transmit_config(USART1, USART_TRANSMIT_ENABLE);
+    usart_enable(USART1);
+}
---
+++ RTE/_GD32F30X_XD/RTE_Components.h
@@ -0,0 +1,69 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GD32F303' 
+ * Target:  'GD32F30X_XD' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32f30x.h"
+
+/* GigaDevice::Device:GD32F30x_StdPeripherals:ADC:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_ADC
+/* GigaDevice::Device:GD32F30x_StdPeripherals:BKP:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_BKP
+/* GigaDevice::Device:GD32F30x_StdPeripherals:CAN:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_CAN
+/* GigaDevice::Device:GD32F30x_StdPeripherals:CRC:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_CRC
+/* GigaDevice::Device:GD32F30x_StdPeripherals:CTC:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_CTC
+/* GigaDevice::Device:GD32F30x_StdPeripherals:DAC:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_DAC
+/* GigaDevice::Device:GD32F30x_StdPeripherals:DBG:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_DBG
+/* GigaDevice::Device:GD32F30x_StdPeripherals:DMA:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_DMA
+/* GigaDevice::Device:GD32F30x_StdPeripherals:ENET:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_ENET
+/* GigaDevice::Device:GD32F30x_StdPeripherals:EXMC:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_EXMC
+/* GigaDevice::Device:GD32F30x_StdPeripherals:EXTI:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_EXTI
+/* GigaDevice::Device:GD32F30x_StdPeripherals:FMC:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_FMC
+/* GigaDevice::Device:GD32F30x_StdPeripherals:FWDGT:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_FWDGT
+/* GigaDevice::Device:GD32F30x_StdPeripherals:GPIO:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_GPIO
+/* GigaDevice::Device:GD32F30x_StdPeripherals:I2C:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_I2C
+/* GigaDevice::Device:GD32F30x_StdPeripherals:MISC:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_MISC
+/* GigaDevice::Device:GD32F30x_StdPeripherals:PMU:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_PMU
+/* GigaDevice::Device:GD32F30x_StdPeripherals:RCU:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_RCU
+/* GigaDevice::Device:GD32F30x_StdPeripherals:RTC:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_RTC
+/* GigaDevice::Device:GD32F30x_StdPeripherals:SDIO:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_SDIO
+/* GigaDevice::Device:GD32F30x_StdPeripherals:SPI_I2S:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_SPI
+/* GigaDevice::Device:GD32F30x_StdPeripherals:TIMER:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_TIMER
+/* GigaDevice::Device:GD32F30x_StdPeripherals:USART:2.1.0 */
+#define RTE_DEVICE_STDPERIPHERALS_USART
+/* GigaDevice::Device:GD32F30x_StdPeripherals:WWDGT:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_WWDGT
+
+
+#endif /* RTE_COMPONENTS_H */
